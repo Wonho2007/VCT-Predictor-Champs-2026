f77a996adf31e57c48060ed065debf848b27c820
Wonho2007 <wonholee2025@gmail.com>
2026-09-23 21:40:33 -0400

    Update comment.

--- scraper.py
+++ scraper.py
@@ -5,5 +5,6 @@ url = "https://www.vlr.gg/team/120/100-thieves"
 response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 soup = BeautifulSoup(response.text, "html.parser")
 #Hi Sid
+#Hi Sid it's wonho again
 
 print(soup.title.text)  # Should print the page title
