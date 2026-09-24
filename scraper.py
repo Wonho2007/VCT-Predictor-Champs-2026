281465a1f516301c2aaae572ed43c08160845802
Wonho2007 <wonholee2025@gmail.com>
2026-09-23 21:38:48 -0400

    Created project.

---
+++ scraper.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.vlr.gg/team/120/100-thieves"
+response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+soup = BeautifulSoup(response.text, "html.parser")
+#Hi Sid
+
+print(soup.title.text)  # Should print the page title
