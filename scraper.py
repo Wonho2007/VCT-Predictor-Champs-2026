cf4faebb9c4fc23ed838a3b147e4d6b73a093b38
sn-kurup <siddharth.n.kurup@gmail.com>
2026-09-23 21:50:05 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 url = "https://www.vlr.gg/team/120/100-thieves"
 response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
 soup = BeautifulSoup(response.text, "html.parser")
-#Hi Sid
-#Hi Sid it's wonho again
-#hi wonho
+
+# https://spotify.link/FK3a7jJnG6b
+
 print(soup.title.text)  # Should print the page title
